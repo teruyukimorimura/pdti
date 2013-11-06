16f898e830c97b42643f9146d7699000a63c03e5
tlahoda <tlahoda@gmail.com>
2013-11-06 14:32:22 -0700

    Added a map for looking up the url for the appropriate wsdl

--- pdti-client/src/main/java/gov/hhs/onc/pdti/client/types/ProviderDirectoryTypes.java
+++ pdti-client/src/main/java/gov/hhs/onc/pdti/client/types/ProviderDirectoryTypes.java
@@ -1,11 +1,35 @@
 package gov.hhs.onc.pdti.client.types;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+
 public enum ProviderDirectoryTypes {
     
     DSML_WSDL("DSML-Based WSDL"), HPDPlus("HPDPlus WSDL");
 
     private String name;
-    
+
+    private static final Map<String, String> urlLookup = new HashMap<String, String>();
+    static {
+        Logger LOGGER = Logger.getLogger(ProviderDirectoryTypes.class);
+        Properties baseProperties = new Properties();
+        Properties properties = new Properties();
+        try {
+            baseProperties.load(ProviderDirectoryTypes.class.getClassLoader().getResourceAsStream("pdti-client.properties"));
+            properties.load(ProviderDirectoryTypes.class.getClassLoader().getResourceAsStream("provider-directory-types.properties"));
+        }
+        catch (Exception e) {
+            LOGGER.debug(e);
+        }
+
+        for (ProviderDirectoryTypes pdt : ProviderDirectoryTypes.values()) {
+            urlLookup.put(pdt.toString(), baseProperties.getProperty("baseAddress") + properties.getProperty(pdt.name()));
+        }
+    }
+
     private ProviderDirectoryTypes(String name) {
         this.name = name;
     }
@@ -13,5 +37,8 @@ public enum ProviderDirectoryTypes {
     public String toString() {
         return name;
     }
-
+    
+    public static String getUrl(String name) {
+        return ProviderDirectoryTypes.urlLookup.get(name);
+    }
 }
