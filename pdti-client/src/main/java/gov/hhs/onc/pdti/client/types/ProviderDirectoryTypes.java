e7459c48c097b2552ea99a5847d87a4e92994bba
tlahoda <tlahoda@gmail.com>
2013-11-15 11:49:54 -0700

    Removed dead code

--- pdti-client/src/main/java/gov/hhs/onc/pdti/client/types/ProviderDirectoryTypes.java
+++ pdti-client/src/main/java/gov/hhs/onc/pdti/client/types/ProviderDirectoryTypes.java
@@ -14,7 +14,6 @@ public enum ProviderDirectoryTypes {
 
     private static final Map<String, String> urlLookup = new HashMap<String, String>();
     static {
-        Logger LOGGER = Logger.getLogger(ProviderDirectoryTypes.class);
         Properties baseProperties = new Properties();
         Properties properties = new Properties();
         try {
@@ -22,11 +21,11 @@ public enum ProviderDirectoryTypes {
             properties.load(ProviderDirectoryTypes.class.getClassLoader().getResourceAsStream("provider-directory-types.properties"));
         }
         catch (Exception e) {
+            Logger LOGGER = Logger.getLogger(ProviderDirectoryTypes.class);
             LOGGER.debug(e);
         }
 
         for (ProviderDirectoryTypes pdt : ProviderDirectoryTypes.values()) {
-            //urlLookup.put(pdt.toString(), baseProperties.getProperty("baseAddress") + properties.getProperty(pdt.name()));
             urlLookup.put(pdt.toString(), properties.getProperty(pdt.name()));
         }
     }
