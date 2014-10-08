2ee43904453eb2b2f5995d080442d19ff24ccee5
Teru Morimura <teruyuki.morimura@iodincorporated.com>
2014-10-08 00:22:27 -0700

    Remove hibernate.cfg.xml and import.sql in pdti-server. It's already available in pdti-dao module.

--- pdti-server/src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE pdtiauditlog (
-    id bigserial primary key,
-    baseDn varchar(200) default NULL,
-	pdRequestType varchar(200) default NULL,
-	status varchar(200) NOT NULL,
-    creationDate timestamp default NULL
-);
